a65358c8ea8e6cfaf3cd044a580767bbe39b7686
wnie <root@NYK-WNIE-L1W.boardbooks.com>
2019-12-08 16:48:49 -0500

    added check

--- stack_data_structure.py
+++ stack_data_structure.py
@@ -9,16 +9,33 @@ class Stack():
     def push(self, item):
         self.items.append(item)
 
-    def pop(self,item):
-        return self.items.pop(item)
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
 
     def get_stack(self):
         return self.items
 
-s = Stack()
-s.push("Hello world!")
-s.push("This if your first stack function.")
-print(s.get_stack())
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
 
-s.pop(0)
-print("after removing hello world", s.get_stack())
+    def clear(self):
+        return self.items.clear()
+
+
+#s = Stack()
+#s.push("Hello world!")
+#s.push("This if your first stack function.")
+#s.push("Testing with more lines")
+#print("originally, we have the following items in the list: ", s.get_stack())
+
+#s.pop()
+#print("\nAfter poping 0", s.get_stack())
+
+#print("\nTake a peek on the last item: ", s.peek())
+
+#s.clear()
+#print("\nAfter clearing the list: ", s.is_empty())
